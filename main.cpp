25b4ac1ebf0042f512a3adf12b11027b96264542
gwegory1 <meszaros.gergo.1@hallgato.ppke.hu>
2022-05-25 19:37:01 +0200

    Main séma a vizsgához

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include "NumberSelect.h"
 #include "DropDownMenu.h"
 #include "GameField.h"
-#include "GameMaster.h"
-#include "button.h"
+#include "Button.h"
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -14,51 +13,25 @@ using namespace std;
 using namespace genv;
 
 
-
 void clear(){
         gout << move_to(0, 0) << color(0, 0, 0) << box(800, 800) << color(255, 255, 255);
     }
 
 
-struct ablak : public window{
-
-    GameField * a1;
-    Button* b1;
-    Button* b2;
-    Button* b3;
-    GameMaster* g;
-    vector<Widget*> widgets;
-
+struct win : public window{
 
     ablak(){
-    g = new GameMaster(7,7);
-    a1   = new GameField(this, 100, 200, 600, 500);
-    b1 = new Button(this, 300, 20, 40, 60, 0, "<", [this]()
-    {
-    g->movepuck_left();
-    a1->load(g);
-    });
-    b2 = new Button(this, 460, 20, 40, 60, 0, ">", [this]()
-    {
-    g->movepuck_right();
-    a1->load(g);
-    });
-    b2 = new Button(this, 350, 20, 100, 60, 0, "GO", [this]()
-    {
-    g->savepuck();
-    g->addpuck();
-    a1->load(g);
-    if(g->check())cout << "win" << endl;
-    });
+     for (Widget * w : widgets) {
+            w->draw();
+        }
+        gout << refresh;
     };
 };
-
 int main()
 {
     gout.open(800,800);
-    event ev;
     gout.load_font("LiberationSans-Regular.ttf",32,true);
-    ablak a;
+    win a;
     a.event_loop();
     return 0;
 }
