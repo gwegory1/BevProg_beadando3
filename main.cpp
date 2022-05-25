9b2e86e3f58808e00c63f86384a01c0d2d37d40b
gwegory1 <meszaros.gergo.1@hallgato.ppke.hu>
2022-05-25 19:49:27 +0200

    main vissza'lliitas

--- main.cpp
+++ main.cpp
@@ -20,13 +20,40 @@ void clear(){
 
 struct win : public window{
 
-    ablak(){
+    GameField * a1;
+    Button* b1;
+    Button* b2;
+    Button* b3;
+    GameMaster* g;
+    vector<Widget*> widgets;
+
+    win(){
+    g = new GameMaster(7,7);
+    a1   = new GameField(this, 100, 200, 600, 500);
+    b1 = new Button(this, 300, 20, 40, 60, 0, "<", [this]()
+    {
+    g->movepuck_left();
+    a1->load(g);
+    });
+    b2 = new Button(this, 460, 20, 40, 60, 0, ">", [this]()
+    {
+    g->movepuck_right();
+    a1->load(g);
+    });
+    b2 = new Button(this, 350, 20, 100, 60, 0, "GO", [this]()
+    {
+    g->savepuck();
+    g->addpuck();
+    a1->load(g);
+    if(g->check())cout << "win" << endl;
+    });
      for (Widget * w : widgets) {
             w->draw();
         }
         gout << refresh;
-    };
+    }
 };
+
 int main()
 {
     gout.open(800,800);
